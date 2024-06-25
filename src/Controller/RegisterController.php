0d7a4301b50e10c6751bf23a525f8006c208c092
SENA <jordan.sena21@gmail.com>
2024-06-25 06:53:13 +0200

    creation form

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -8,11 +8,9 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class RegisterController extends AbstractController
 {
-    #[Route('/register', name: 'app_register')]
+    #[Route('/inscription', name: 'app_register')]
     public function index(): Response
     {
-        return $this->render('register/index.html.twig', [
-            'controller_name' => 'RegisterController',
-        ]);
+        return $this->render('register/index.html.twig');
     }
 }

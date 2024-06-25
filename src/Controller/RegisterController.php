e858505f4be566b3ca12bc8df8507bd37b9f84a5
SENA <jordan.sena21@gmail.com>
2024-06-25 22:55:12 +0200

    updates form

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,17 +2,32 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Form\RegisterUserType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class RegisterController extends AbstractController
 {
     #[Route('/inscription', name: 'app_register')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManagerInterface): Response
     {
-        $form = $this->createForm(RegisterUserType::class);
-        return $this->render('register/index.html.twig', ['registerForm' => $form->createView()]);
+        $user = new User();
+        $form = $this->createForm(RegisterUserType::class, $user);
+
+        //écoute la request, ce que soumet l'utilisateur dans le formulaire
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManagerInterface->persist($user);
+            $entityManagerInterface->flush();
+        }
+
+        return $this->render('register/index.html.twig', [
+            'registerForm' => $form->createView()
+        ]);
     }
 }

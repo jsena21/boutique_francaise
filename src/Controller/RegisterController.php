0a127a5d5aacffbda1dc721fd0f13f99f2ee48c0
SENA <jordan.sena21@gmail.com>
2024-06-25 07:11:19 +0200

    updates form

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Form\RegisterUserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,6 +12,7 @@ class RegisterController extends AbstractController
     #[Route('/inscription', name: 'app_register')]
     public function index(): Response
     {
-        return $this->render('register/index.html.twig');
+        $form = $this->createForm(RegisterUserType::class);
+        return $this->render('register/index.html.twig', ['registerForm' => $form->createView()]);
     }
 }
--- src/Form/RegisterUserType.php
+++ src/Form/RegisterUserType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,11 +14,10 @@ class RegisterUserType extends AbstractType
     {
         $builder
             ->add('email')
-            ->add('roles')
             ->add('password')
             ->add('firstname')
             ->add('lastname')
-        ;
+            ->add('submit', SubmitType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
